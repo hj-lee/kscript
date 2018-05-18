e8f6aced5e3dcae7e0aedae009acdea313da8a84
Hwijae Lee <hj.d.lee@gmail.com>
2018-05-18 15:26:12 -0400

    Temporal - copy kscript and kscript.jar to projectDir

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,14 @@ val shadowJar by tasks.getting(ShadowJar::class) {
             from(File(projectDir, "src/kscript"))
             into(archivePath.parentFile)
         }
+        copy {
+            from(archivePath)
+            into(projectDir)
+        }
+        copy {
+            from(File(projectDir, "src/kscript"))
+            into(projectDir)
+        }
     }
 }
 
